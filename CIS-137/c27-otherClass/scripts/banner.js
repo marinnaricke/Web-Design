5ce2115f4fbc6141a580b5c419ab12ef53a5676f
marinnaricke <32140350+marinnaricke@users.noreply.github.com>
2021-04-27 20:18:56 -0400

    Add files via upload

---
+++ CIS-137/c27-otherClass/scripts/banner.js
@@ -0,0 +1,19 @@
+﻿
+// Banner Ad Functions
+function displayAds()
+{
+    var timer = setInterval("showBannerAd();", 5000);
+}
+
+function showBannerAd()
+{
+    //Create a random number  (to get upper range - add together and subtract 1)
+    var myRandomNumber;
+    myRandomNumber = Math.floor(Math.random() * 5) + 1;  //RN between 1 and 5
+    //window.alert("Random Number is " + myRandomNumber);
+    document.getElementById("ImageBanner").src = "images/Nature" +
+                myRandomNumber + ".PNG";
+}
+
+// Other functions
+
